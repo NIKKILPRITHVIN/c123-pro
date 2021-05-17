a5941210139ec455bdfc5234a5c8cef7eeeb5bc7
aaryan456 <62750261+aaryan456@users.noreply.github.com>
2021-05-17 10:22:26 +0530

    Update c.py

--- c.py
+++ c.py
@@ -10,23 +10,21 @@ from PIL import Image
 import PIL.ImageOps
 
 
-#Fetching the data
+
 X = np.load('image.npz')['arr_0']
 y = pd.read_csv("labels.csv")["labels"]
 print(pd.Series(y).value_counts())
 classes = ['A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J', "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 nclasses = len(classes)
-
-#Splitting the data and scaling it
+f
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=9, train_size=7500, test_size=2500)
-#scaling the features
+
 X_train_scaled = X_train/255.0
 X_test_scaled = X_test/255.0
 
-#Fitting the training data into the model
+
 clf = LogisticRegression(solver='saga', multi_class='multinomial').fit(X_train_scaled, y_train)
 
-#Calculating the accuracy of the model
 y_pred = clf.predict(X_test_scaled)
 accuracy = accuracy_score(y_test, y_pred)
 print("The accuracy is :- ",accuracy)
@@ -39,10 +37,8 @@ while(True):
   try:
     ret, frame = cap.read()
 
-    # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    #Drawing a box in the center of the video
     height, width = gray.shape
     upper_left = (int(width / 2 - 56), int(height / 2 - 56))
     bottom_right = (int(width / 2 + 56), int(height / 2 + 56))
